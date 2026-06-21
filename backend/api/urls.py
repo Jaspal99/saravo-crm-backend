c6642e584b3923913fe6199b83c266619015b395
Jaspal99 <Khalsajaspalsingh6@gmail.com>
2026-06-21 17:41:15 +0530

    user specific

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todo/',views.TodoListCreateView.as_view()),
+    path('',views.RootAPIView.as_view(),name='root'),
+    path('todo/',views.TodoListCreateView.as_view(),name='list'),
     path('todo/<int:pk>',views.TodoRetrieveUpdateDestroyView.as_view())
 ]
